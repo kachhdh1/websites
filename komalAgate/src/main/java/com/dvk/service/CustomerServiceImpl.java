5a4ee256ce7b2f0ffd257b4d97dc39a09730ef9a
kachhdh1 <dharmik.kachhia@yahoo.com>
2016-06-26 07:43:40 +0530

    Password decryption and spring security tag
    Added application password decryption functionality.
    Added spring security tag support for jsp.

--- komalAgate/src/main/java/com/dvk/service/CustomerServiceImpl.java
+++ komalAgate/src/main/java/com/dvk/service/CustomerServiceImpl.java
@@ -3,7 +3,9 @@ package com.dvk.service;
 import com.dvk.dao.CustomerDao;
 import com.dvk.model.Customer;
 import com.dvk.service.CustomerService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +15,14 @@ public class CustomerServiceImpl implements CustomerService{
 
     @Autowired
     private CustomerDao customerDao;
+    
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
     public void addCustomer (Customer customer) {
+    	//added logic for encoding password
+    	String encodedPass = encoder.encode(customer.getPassword());
+    	customer.setPassword(encodedPass);
         customerDao.addCustomer(customer);
     }
 
